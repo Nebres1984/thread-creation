8abfd265207d482c795ea1639b74e065107b6966
Nebres <nebres84@hotmail.com>
2023-03-02 13:49:44 +0100

    Commit one

---
+++ src/MultiExcutor/MultiExecutor.java
@@ -0,0 +1,29 @@
+package MultiExcutor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiExecutor {
+
+    // Add any necessary member variables here
+    List<Runnable> tasks;
+    /*
+     * @param tasks to executed concurrently
+     */
+    public MultiExecutor(List<Runnable> tasks) {
+        this. tasks = tasks;
+    }
+
+    /**
+     * Starts and executes all the tasks concurrently
+     */
+    public void executeAll() {
+        List<Thread> taskList = new ArrayList<>();
+        tasks.forEach(task -> {
+           Thread thread = new Thread(task);
+           taskList.add(thread);
+        });
+
+        taskList.forEach(Thread::run);
+    }
+}
